655fee1e9633cd3d3f110ba8cc3a46653460ac8d
Sanghyuk Park <dropthegit@gmail.com>
2023-11-29 15:49:48 +0900

    Setting

---
+++ src/main/java/com/ll/sb231129/Sb231129Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb231129;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb231129Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb231129Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sb231129/Sb231129ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb231129;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb231129ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
